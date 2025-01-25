534b3cf7d83ba89d1ad74a353dbaf71d3d9c96fa
JesusBlan <jezublanco@gmail.com>
2025-01-25 11:47:36 +0100

    changed the console log of the bookInfo function for a return

--- scripts.js
+++ scripts.js
@@ -4,8 +4,6 @@ function book(title, autor, pages, read){
     this.pages = pages;
     this.read = read;
     this.bookInfo = function(){
-        console.log(
-            "Book: "+ this.title + ", written by " + this.autor + " has " + this.pages + " pages. Read = "this.read
-        )
+    return "Book: "+ this.title + ", written by " + this.autor + " has " + this.pages + " pages. Read = "+this.read;
     }
 }
